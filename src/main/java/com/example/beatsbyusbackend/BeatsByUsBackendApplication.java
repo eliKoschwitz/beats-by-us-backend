ce37512d462d0ce25ad5fb1ae4ca469ec50de34d
elikoschwitz <e.koschwitz@googmail.com>
2023-01-06 10:34:42 +0100

    Init: big bang

---
+++ src/main/java/com/example/beatsbyusbackend/BeatsByUsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.beatsbyusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatsByUsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeatsByUsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/beatsbyusbackend/BeatsByUsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beatsbyusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatsByUsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
